b83fa16c3360a172420fa9908f10f0430ed2707b
rtupclry <130798012+rtupclry@users.noreply.github.com>
2023-09-06 19:07:29 -0300

    Update logica de programacao 4

---
+++ estrutura decisao.py
@@ -0,0 +1,30 @@
+
+# numero1 = float(input("Digite o primeiro número: "))
+# numero2 = float(input("Digite o segundo número: "))
+
+# if numero1 > numero2:
+#     print("O primeiro número é maior")
+# elif numero1 < numero2:
+#     print("O segundo número é maior")
+# else:
+#     print("Os dois números são iguais"
+
+valor = float(input("Digite o valor do positivo: "))
+
+if valor > 0:
+    print("O valor é positivo")
+elif valor < 0:
+    print("O valor é negativo")
+else:
+    print("O valor é nulo")
+
+
+
+valor = float(input("Digite o valor do positivo: "))
+
+if valor > 0:
+    print("O valor é positivo")
+elif valor < 0:
+    print("O valor é negativo")
+else:
+    print("O valor é nulo")
---
+++ funcao em pytron.py
@@ -0,0 +1,43 @@
+
+produtor = ["ABC123", "abd12","ABS111","AbB222"]
+
+texto = "abd012"
+
+texto = texto.upper()
+texto = texto.strip()
+print(texto)
+
+def area(base, altura):
+    area = base * altura
+    print(area)
+    return area
+
+
+area(10, 20)
+
+def tempformula(f):
+    temp = (f - 32) * (5 / 9 )
+    print(temp)
+    return temp
+
+
+tempformula(100)
+
+
+def soma():
+    x = float(input("Digite um numero: "))
+    y = float(input("Digite outro numero: "))
+    soma = x + y
+print("soma", soma)
+
+continuar= 1
+while continuar:
+   if(continuar):
+       soma()
+continuar = int(input("Digite 0 se desejar encerrar ou qualquer outro numero para continuar "))
+
+def funcao(dia,mes, ano):
+    data
+
+
+
